fb767bc59a1556ae5714f7ae47d88170b1172968
AliMcdad <alimcdad0@gmail.com>
2023-03-20 08:58:50 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { MatIconModule } from '@angular/material/icon';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatIconModule,
+
+    AppRoutingModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyC0AMX1hMW3fe0zNdh2_VFFzi5E4qwZfmQ",
+        authDomain: "taeaqub-93e0d.firebaseapp.com",
+        projectId: "taeaqub-93e0d",
+        storageBucket: "taeaqub-93e0d.appspot.com",
+        messagingSenderId: "421252181056",
+        appId: "1:421252181056:web:6c4d2e22eab1d678d7c3a5"
+
+    }
+}
